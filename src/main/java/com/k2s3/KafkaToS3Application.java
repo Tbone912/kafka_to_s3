6cbe395c93392656fc41f5d62702661273ce5c98
Tbone912 <56204801+Tbone912@users.noreply.github.com>
2023-04-20 14:12:31 -0400

    Postman upload to S3 works!

--- src/main/java/com/k2s3/KafkaToS3Application.java
+++ src/main/java/com/k2s3/KafkaToS3Application.java
@@ -1,8 +1,12 @@
 // https://howtodoinjava.com/kafka/spring-boot-with-kafka/
+// https://www.baeldung.com/aws-s3-java
+//
 // bin\windows\zookeeper-server-start.bat config\zookeeper.properties
 // bin\windows\kafka-server-start.bat .\config\server.properties
 // bin\windows\kafka-console-consumer.bat --bootstrap-server 0:9092 --topic test --from-beginning
+// bin\windows\kafka-topics.bat --bootstrap-server=localhost:9092 --list
 //
+
 package com.k2s3;
 
 import org.springframework.boot.SpringApplication;
@@ -13,6 +17,7 @@ public class KafkaToS3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaToS3Application.class, args);
+
 	}
 
 }
--- src/main/java/com/k2s3/service/KafkaConsumerService.java
+++ src/main/java/com/k2s3/service/KafkaConsumerService.java
@@ -1,5 +1,10 @@
 package com.k2s3.service;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -8,9 +13,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumerService {
 	private final Logger logger = LoggerFactory.getLogger(KafkaConsumerService.class);
+	public LocalDateTime localDateTime = LocalDateTime.now();
+	public String FileDate = localDateTime.toString();
 
 	@KafkaListener(topics = "test", groupId = "group_id")
-	public void consume(String message) {
+	public void consume(String message) throws IOException {
+
 		logger.info(String.format("Message recieved -> %s", message));
+		
+
+		// Writes Kafka data to text file for S3
+		File myFile = new File("toS3.txt");
+		FileWriter s3File = new FileWriter("toS3.txt");
+		s3File.write(message);
+		s3File.close();
 	}
 }
